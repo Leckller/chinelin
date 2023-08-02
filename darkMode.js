7eb6777a1bb7d2dd755643786e58720d3f6c4a06
Leckller <7leckler@gmail.com>
2023-08-02 19:51:04 -0300

    feat 50% darkmode

--- darkMode.js
+++ darkMode.js
@@ -2,7 +2,21 @@ const btn = document.querySelector('#dark');
 const corpo = document.querySelector('body');
 const icon = document.querySelector('#i');
 
+function verifyToF () {
+  if (btn.checked) {
+    icon.className = "bi bi-sun-fill"
+    corpo.style.background = '#eaeaea'
+  }
+  else {
+    icon.className = "bi bi-moon-fill"
+    corpo.style.background = '#222'
+  }
+}
+
 btn.addEventListener('click', (e) => {
-  if (btn.checked) icon.className = "bi bi-moon-fill"
-  else icon.className = "bi bi-sun-fill"
+  verifyToF()
 })
+
+window.onload = () => {
+  verifyToF()
+}
