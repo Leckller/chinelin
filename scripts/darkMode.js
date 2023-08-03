b96f1f2d536bd14de440c166b2c9f9ba906c1966
Leckller <7leckler@gmail.com>
2023-08-02 22:15:21 -0300

    reiniciando main

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,15 +1,18 @@
 const btn = document.querySelector('#dark');
 const corpo = document.querySelector('body');
 const icon = document.querySelector('#i');
+const main = document.querySelector('main')
 
 function verifyToF () {
   if (btn.checked) {
     icon.className = "bi bi-sun-fill"
     corpo.className = 'light'
+    main.style.cssText -= 'animation: goToRight 1.5s forwards'
   }
   else {
     icon.className = "bi bi-moon-fill"
     corpo.className = 'dark'
+    main.style.cssText += 'animation: goToRight 1.5s forwards'
   }
 }
 
